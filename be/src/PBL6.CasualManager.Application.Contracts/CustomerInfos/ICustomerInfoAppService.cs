f61fd0fd247b6a075b104208749655da5a889981
Lê Minh Lợi <loileminh0410@gmail.com>
2022-11-03 08:09:55 +0000

    Revert "Merge branch 'feat/PBL6-10-add-service-to-register-for-worker-and-customer' into 'develop'"
    This reverts merge request !8

--- be/src/PBL6.CasualManager.Application.Contracts/CustomerInfos/ICustomerInfoAppService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace PBL6.CasualManager.CustomerInfos
-{
-    public interface ICustomerInfoAppService
-    {
-        Task<bool> AddAsync(CustomerInfoDto customerInfosDto);
-    }
-}
