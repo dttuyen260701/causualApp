bd8515dba65bfd97b627525fc9b69aea0c4e9f73
“NhiLuu” <nhi.luu@team.enouvo.com>
2022-11-05 16:40:41 +0700

    PBL6-40 update UI for edit info user

--- mobile/app/src/main/java/com/example/pbl6app/fragment/ProfileFragment.java
+++ mobile/app/src/main/java/com/example/pbl6app/fragment/ProfileFragment.java
@@ -13,6 +13,9 @@ import androidx.fragment.app.Fragment;
 
 import com.example.pbl6app.databinding.FragmentProfileBinding;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class ProfileFragment extends Fragment {
 
     private FragmentProfileBinding binding;
@@ -27,5 +30,42 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        enableEdit();
+        saveData();
+        setHintDate();
+    }
+
+    void enableEdit(){
+        binding.imvEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.imvEdit.setVisibility(View.GONE);
+                binding.imvSave.setVisibility(View.VISIBLE);
+                binding.edtAddress.setEnabled(true);
+                binding.edtAge.setEnabled(true);
+                binding.edtName.setEnabled(true);
+                binding.edtPhone.setEnabled(true);
+            }
+        });
+    }
+
+    void saveData(){
+        binding.imvSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.imvEdit.setVisibility(View.VISIBLE);
+                binding.imvSave.setVisibility(View.GONE);
+                binding.edtAddress.setEnabled(false);
+                binding.edtAge.setEnabled(false);
+                binding.edtName.setEnabled(false);
+                binding.edtPhone.setEnabled(false);
+            }
+        });
+    }
+
+    void setHintDate(){
+        String timeStamp = new SimpleDateFormat("dd/MM/yyyy").format(Calendar.getInstance().getTime());
+        binding.edtAge.setHint(timeStamp);
     }
 }
